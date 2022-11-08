f392a6cb33bc7559926485db95ad2bf37420fc37
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-08 19:51:20 +0100

    Commit 2

---
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/skypeclone/DashBoardActivity.java
@@ -0,0 +1,14 @@
+package whatsappclone.proyecto_javier_juan_uceda.skypeclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+    }
+}
