d4f428f88a3b53a50706b8d2ec100a561c2377d3
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-10 10:16:26 +0100

    Commit 5

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/skypeclone/MainActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/skypeclone/MainActivity.java
@@ -1,14 +1,74 @@
 package whatsappclone.proyecto_javier_juan_uceda.skypeclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+    private TextView tvNewUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUI();
+    }
+
+    private void setUI() {
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnRegister);
+        tvNewUser = findViewById(R.id.tvRegister);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                mAuth.signInWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Timber.tag(RegistrationActivity.class.getSimpleName()).i("Firebase registration succesful");
+                        Toast.makeText(MainActivity.this, "Login succesful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, DashBoardActivity.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                        Timber.tag(MainActivity.class.getSimpleName()).e("Firebase login error: %s", e.getMessage());
+
+                    }
+                });
+            }
+        });
+
+        tvNewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/skypeclone/RegistrationActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/skypeclone/RegistrationActivity.java
@@ -1,15 +1,27 @@
 package whatsappclone.proyecto_javier_juan_uceda.skypeclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.PaintDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import timber.log.Timber;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -18,6 +30,8 @@ public class RegistrationActivity extends AppCompatActivity {
     private TextView tvAlreadyRegistered;
 
     private FirebaseAuth auth;
+    private ProgressDialog progressDialog;
+    private FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +47,58 @@ public class RegistrationActivity extends AppCompatActivity {
         btnRegister = findViewById(R.id.btnRegister);
         tvAlreadyRegistered = findViewById(R.id.tvDontHaveAccount);
 
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registration in progress");
+
+        database = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressDialog.show();
                 String name = etName.getText().toString();
                 String email = etEmail.getText().toString();
                 String password = etPassword.getText().toString();
+
+                User user = new User();
+                user.setName(name);
+                user.setEmail(email);
+                user.setPassword(password);
+
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            database.collection("Users").document().set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    progressDialog.dismiss();
+                                    Timber.tag(RegistrationActivity.class.getSimpleName()).i("Firebase registration succesful");
+                                    Toast.makeText(RegistrationActivity.this, "Registration succesful", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(RegistrationActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                                    Timber.tag(RegistrationActivity.class.getSimpleName()).e("Firebase registration error: %s", e.getMessage());
+                                }
+                            });
+
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegistrationActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                        Timber.tag(RegistrationActivity.class.getSimpleName()).e("Firebase registration error: " + e.getMessage());
+
+                    }
+                });
+
             }
         });
 
