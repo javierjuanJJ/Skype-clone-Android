e892b76ffa5d9c27c3f607451a35d0b58db4d52a
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-11-11 19:54:11 +0100

    Commit 6

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/skypeclone/DashBoardActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/skypeclone/DashBoardActivity.java
@@ -34,14 +34,44 @@ public class DashBoardActivity extends AppCompatActivity {
         try {
             URL serverURL;
 
-            serverURL = new URL("https://meet.jit.si");
-            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder().setServerURL(serverURL).build();
-            JitsiMeet.setDefaultConferenceOptions(options);
+              serverURL = new URL("https://meet.jit.si");
+//            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder().setServerURL(serverURL).setFeatureFlag("welcomepage.enabled", false).build();
+//            JitsiMeet.setDefaultConferenceOptions(options);
+
+
+// Somewhere early in your app.
+            JitsiMeetConferenceOptions defaultOptions
+                    = new JitsiMeetConferenceOptions.Builder()
+                    .setServerURL(serverURL)
+                    // When using JaaS, set the obtained JWT here
+                    //.setToken("MyJWT")
+                    // Different features flags can be set
+                    // .setFeatureFlag("toolbox.enabled", false)
+                    // .setFeatureFlag("filmstrip.enabled", false)
+                    .setFeatureFlag("welcomepage.enabled", false)
+                    .build();
+
+
             btnJoin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    JitsiMeetConferenceOptions m_options = new JitsiMeetConferenceOptions.Builder().setRoom(etCodeInput.getText().toString()).build();
-                    JitsiMeetActivity.launch(getApplicationContext(), m_options);
+//                    JitsiMeetConferenceOptions m_options = new JitsiMeetConferenceOptions.Builder().setRoom(etCodeInput.getText().toString()).build();
+//                    JitsiMeetActivity.launch(getApplicationContext(), m_options);
+
+// Build options object for joining the conference. The SDK will merge the default
+// one we set earlier and this one when joining.
+                    JitsiMeetConferenceOptions options
+                            = new JitsiMeetConferenceOptions.Builder()
+                            .setRoom(etCodeInput.getText().toString())
+                            // Settings for audio and video
+                            //.setAudioMuted(true)
+                            //.setVideoMuted(true)
+                            .build();
+// Launch the new activity with the given options. The launch() method takes care
+// of creating the required Intent and passing the options.
+                    JitsiMeetActivity.launch(getApplicationContext(), options);
+
+
                 }
             });
         } catch (MalformedURLException e) {
